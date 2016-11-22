ab6dc8ff9361927a39f589387fb679b6212734f5
Paul Adams <Paul Adams>
2016-11-21 23:21:56 -0500

    fixed get request

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div id="app">
-
+    <ul>
+      <li v-for="currency in currencies">
+          {{currency.id}} - {{currency.curr}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import forIn from 'lodash'
 export default {
   name: 'app',
   created(){
+    var self = this
     axios.get('http://api.fixer.io/latest')
     .then(function (response) {
-      console.log(response.data.rates);
+     _.forIn(response.data.rates, function(value, key) {
+        console.log(key, value);
+        self.currencies.push({id: key, curr: value})
+      })
     })
     .catch(function (error) {
       console.log(error);
@@ -19,6 +28,7 @@ export default {
   },
   data () {
     return {
+      currencies: []
     }
   }
 }
