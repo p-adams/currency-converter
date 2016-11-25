89d2e23d202585b7bc079853ab44fe9449d20693
Paul Adams <Paul Adams>
2016-11-25 00:56:11 -0500

    added some UI elements

--- src/App.vue
+++ src/App.vue
@@ -5,101 +5,61 @@
           {{currency.id}} - {{currency.curr}}
       </li>
     </ul>-->
-    <h1>Currency Coverter</h1>
+
+      <el-menu title="Currency Converter">
+        <h1>Currency Coverter</h1>
+      </el-menu>
     
-      <!--<table>
-        <tr>
-          <td>
-            <input type="text">
-          </td>
-          <td>
-            <select>
-                <option v-for="currency in currencies">{{currency.id}}</option>
-            </select>
-          </td>
-          <td>
-             <select>
-                <option v-for="currency in currencies">{{currency.id}}</option>
-            </select>
-          </td>
-        </tr>
-      </table>-->
-
-      <el-table
-        :data="currencyData"
-        border
-        style="width: 100%">
-
-  <!-- curr input 
-    <el-table-column
-      inline-template
-      label="Date"
-      width="180">
-      <div>
-        <el-icon name="time"></el-icon>
-        <span style="margin-left: 10px">{{ row.date }}</span>
-      </div>
-    </el-table-column>
-  --> 
-
-
-  <!-- curr A
-     <el-table-column
-      :context="_self"
-      inline-template
-      label="Operations">
-      <div>
-        <el-select v-model="value" placeholder="Select">
-          <el-option
-          v-for="currency in currencies"
-          :label="currency.id"
-          :value="currency.curr">
-          </el-option>
-        </el-select>
-      </div>
-    </el-table-column>
--->
-
-<!-- curr B 
-    <el-table-column
-      inline-template
-      label="Name"
-      width="180">
-      <el-popover trigger="hover" placement="top">
-        <p>Name: {{ row.name }}</p>
-        <p>Addr: {{ row.address }}</p>
-        <div slot="reference">
-          <el-tag>{{ row.name }}</el-tag>
+  
+      <el-row :gutter="20">
+      <el-col :span="16">
+        
+        <div class="grid-content bg-purple">
+                    <el-form :inline="true" :model="formInline" class="demo-form-inline">
+        <el-form-item>
+          <el-input v-model="curr"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-select v-model="curra" placeholder="choose currency">
+            <el-option v-for="currency in currencyData"
+                       :label="currency.id" 
+                       :value="currency.curr">
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-select v-model="currb" placeholder="choose currency">
+            <el-option v-for="currency in currencyData"
+                       :label="currency.id" 
+                       :value="currency.curr">
+            </el-option>
+          </el-select>
+        </el-form-item>
+        
+        
+        
+        <el-form-item>
+          <el-button type="primary"><i class="el-icon-arrow-right"></i></el-button>
+        </el-form-item>
+      </el-form>
+        
+        
+        
         </div>
-      </el-popover>
-    </el-table-column>
+      </el-col>
 
--->
+      <el-col :span="8">
+        <div class="grid-content bg-purple">
+          <h3>Popular currencies</h3>
+          </div>
+          </el-col>
+      </el-row>
 
-<!-- convert 
 
-    <el-table-column
-      :context="_self"
-      inline-template
-      label="Operations">
-      <div>
-        <el-button
-          size="small"
-          @click="handleEdit($index, row)">
-          Edit
-        </el-button>
-        <el-button
-          size="small"
-          type="danger"
-          @click="handleDelete($index, row)">
-          Delete
-        </el-button>
-      </div>
-    </el-table-column>
 
--->
 
-  </el-table>
+
+
 
   </div>
 </template>
@@ -114,7 +74,6 @@ export default {
     axios.get('http://api.fixer.io/latest')
     .then(function (response) {
      _.forIn(response.data.rates, function(value, key) {
-        //console.log(key, value);
         self.currencyData.push({id: key, curr: value})
       })
     })
@@ -125,27 +84,26 @@ export default {
   data () {
     return {
       currencyData: [{id: '', curr: ''}],
-        value: '',
+        curr: '',
+        curra: '',
+        currb: ''
     }
   },
     methods: {
-      handleEdit(index, row) {
-        console.log(index, row);
-      },
-      handleDelete(index, row) {
-        console.log(index, row);
-      }
+    
   }
 }
 </script>
-
 <style>
-  table, tr, td{
-    width: 50%;
-    height: 50%;
-    border: 1px solid black;
+  .el-menu{
+    background-color: #20A0FF;
+    margin-bottom: 25px;
   }
-  #table{
-    border: 1px solid blue;
+
+  h1{
+      margin-left: 10px;
+      font-family: 'helvetica';
+      color: ghostwhite;
+      font-size: 20px large;
   }
 </style>
