0c6908001115887728ae4f0e8646f35eabffd505
Paul Adams <Paul Adams>
2016-11-26 21:00:38 -0500

    Updated App component

--- src/App.vue
+++ src/App.vue
@@ -3,42 +3,105 @@
       <el-menu title="Currency Converter">
         <h1>Currency Coverter</h1>
       </el-menu>
-      <el-row :gutter="20">
-      <el-col :span="16">
+      <el-row :gutter="18">
+      <el-col :span="13">
         <div class="grid-content bg-purple">
           <el-form :inline="true" :model="formInline" class="demo-form-inline">
         <el-form-item>
-          <el-input v-model="curr"></el-input>
+          <el-input v-model.number="curr"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-select v-model="curra" @change="loadConversions" placeholder="choose currency">
+          <el-select v-model="currA" @change="loadConversions" placeholder="choose currency">
             <el-option v-for="currency in currencyData"
                        :value="currency.id">{{currency.id}} - {{currency.curr}}
             </el-option>
           </el-select>
         </el-form-item>
         <el-form-item>
-          <el-select v-model="currb" placeholder="choose currency">
-            <el-option  v-for="currency in conv"
+          <el-select v-model="currB" placeholder="choose currency">
+            <el-option v-for="currency in conv"
                       :label="currency.id"
-                      :value="currency.curr">
+                      :value="currency.curr"
+                      
+                      >
             </el-option>
           </el-select>
-        </el-form-item>
-        
+        </el-form-item>    
         <el-form-item>
-          <el-button type="primary"><i class="el-icon-arrow-right"></i></el-button>
+          <el-button type="primary" @click="convert"><i class="el-icon-arrow-right"></i></el-button>
         </el-form-item>
-      </el-form>    
+      </el-form>
+
+      <el-table v-if="clicked"
+          :data="cc"
+          style="width: 100%">
+          <el-table-column
+            prop="amount"
+            label="Amount"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="base"
+            label="Base currency"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="quote"
+            label="Quote currency">
+          </el-table-column>
+          <el-table-column
+            prop="total"
+            label="Total">
+          </el-table-column>
+    </el-table>
+
+
+
+
+
+
+
+
         </div>
       </el-col>
-      <el-col :span="8">
+      <el-col :span="11">
         <div class="grid-content bg-purple">
           <h3>Popular currencies</h3>
-          </div>
-          </el-col>
+            <el-table
+              :data="popularCurrencies"
+              border
+              style="width: 100%">
+              <el-table-column       
+                label="Symbol"
+                prop="symbol"
+                width="150">
+              </el-table-column>
+              <el-table-column   
+                label="Code"
+                prop="code"
+                width="150">
+            </el-table-column>
+          <el-table-column
+              prop="name"
+              label="Name"
+              
+              >
+              </div>
+            </el-table-column>
+          </el-table>
+        </div>
+        </el-col>
       </el-row>
-      {{convert}}
+     
+
+     
+
+
+
+
+
+
+
   </div>
 </template>
 
@@ -46,31 +109,52 @@
 import axios from 'axios'
 import forIn from 'lodash'
 import codes from './currency.js'
+import pop from './pop.js'
 
 export default {
   name: 'app',
   created(){
     var self = this
     codes.map(code => {
-      self.currencyData.push(
-        {id: code.code, curr: code.name}
-        )
+      self.currencyData.push({
+        id: code.code,
+        curr: code.name
+      })
     }) 
-    
+    this.loadPop() 
   },
   data () {
     return {
       currencyData: [],
       conv: [],
-        curr: '',
-        curra: '',
-        currb: ''
+      popularCurrencies: [],
+        curr: 1,
+        currA: '',
+        currB: '',
+        currId: '',
+        cc:[],
+        clicked: false
     }
   },
     methods: {
+      convert(){
+        this.clicked = true
+        var self = this
+        var opt = ''
+        this.conv.map(c => {
+          c.curr===self.currB ? opt = c.id : null
+        })
+        this.cc = [{
+          amount :this.curr,
+          base : this.currA,
+          quote : opt,
+          total : (this.curr * this.currB).toFixed(2)
+        }]
+
+      },
       loadConversions(){
         var self = this
-        axios.get('http://api.fixer.io/latest?base=' + this.curra)
+        axios.get('http://api.fixer.io/latest?base=' + this.currA)
         .then(function (response) {
         _.forIn(response.data.rates, function(value, key) {
             self.conv.push({id: key, curr: value})
@@ -80,33 +164,17 @@ export default {
           console.log(error);
         });
         this.conv = []
-      }     
-  },
-  computed: {
-   convert(){
-    
-      return (this.curr * this.currb).toFixed(2)
-    },
-    a(){
-      var self = this
-      var sel = ""
-      this.currencyData.map(c=>{
-        if(c.curr === self.curra){
-           sel = c.id
-        } 
-      })
-      return sel
-    },
-    b(){
-      var self = this
-      var sel = ""
-      this.currencyData.map(c=>{
-        if(c.curr === self.currb){
-           sel = c.id
-        } 
-      })
-      return sel
-    }
+      },
+      loadPop(){
+        var self = this
+        pop.map(currency => {
+          self.popularCurrencies.push({
+            name: currency.name,
+            code: currency.code,
+            symbol: currency.symbol
+          })
+        })
+      }
   }
 }
 </script>
@@ -115,7 +183,11 @@ export default {
     background-color: #20A0FF;
     margin-bottom: 25px;
   }
-
+  h3{
+    font-family: 'helvetica';
+    color: gray;
+    text-align: center;
+  }
   h1{
       margin-left: 10px;
       font-family: 'helvetica';
---
+++ src/pop.js
@@ -0,0 +1,85 @@
+const pop_curr = [
+    {
+        "symbol": "$",
+        "name": "US Dollar",
+        "symbol_native": "$",
+        "decimal_digits": 2,
+        "rounding": 0,
+        "code": "USD",
+        "name_plural": "US dollars"
+    },
+      {
+        "symbol": "€",
+        "name": "Euro",
+        "symbol_native": "€",
+        "decimal_digits": 2,
+        "rounding": 0,
+        "code": "EUR",
+        "name_plural": "euros"
+    },
+    {
+        "symbol": "AU$",
+        "name": "Australian Dollar",
+        "symbol_native": "$",
+        "decimal_digits": 2,
+        "rounding": 0,
+        "code": "AUD",
+        "name_plural": "Australian dollars"
+    },
+     {
+        "symbol": "CN¥",
+        "name": "Chinese Yuan",
+        "symbol_native": "CN¥",
+        "decimal_digits": 2,
+        "rounding": 0,
+        "code": "CNY",
+        "name_plural": "Chinese yuan"
+    },
+     {
+        "symbol": "£",
+        "name": "British Pound Sterling",
+        "symbol_native": "£",
+        "decimal_digits": 2,
+        "rounding": 0,
+        "code": "GBP",
+        "name_plural": "British pounds sterling"
+    },
+     {
+        "symbol": "Rp",
+        "name": "Indonesian Rupiah",
+        "symbol_native": "Rp",
+        "decimal_digits": 0,
+        "rounding": 0,
+        "code": "IDR",
+        "name_plural": "Indonesian rupiahs"
+    },
+      {
+        "symbol": "¥",
+        "name": "Japanese Yen",
+        "symbol_native": "￥",
+        "decimal_digits": 0,
+        "rounding": 0,
+        "code": "JPY",
+        "name_plural": "Japanese yen"
+    },
+    {
+        "symbol": "RM",
+        "name": "Malaysian Ringgit",
+        "symbol_native": "RM",
+        "decimal_digits": 2,
+        "rounding": 0,
+        "code": "MYR",
+        "name_plural": "Malaysian ringgits"
+    },
+     {
+        "symbol": "RUB",
+        "name": "Russian Ruble",
+        "symbol_native": "руб.",
+        "decimal_digits": 2,
+        "rounding": 0,
+        "code": "RUB",
+        "name_plural": "Russian rubles"
+    },
+]
+
+export default pop_curr
